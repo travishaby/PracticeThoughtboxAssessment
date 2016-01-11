51c8d2143f5af35ae6fb87f28bf7980951e24ea1
Travis Haby <travishaby@gmail.com>
2016-01-11 13:32:35 -0700

    feature/pass test: Error message displays when link is created with an invalid url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:error] = "Account creation unsuccessful: #{@user.errors}"
+      flash[:error] = @user.errors.full_messages.join(", ")
       redirect_to new_user_path
     end
   end
